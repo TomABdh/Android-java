bae4931ee3f06ff86ddd7db014a8886955422956
Zhu Pengming <89885722+TomABdh@users.noreply.github.com>
2023-06-07 13:28:05 +0800

    Add files via upload

---
+++ java/com/example/mstu1/DetailAccountActivity.java
@@ -0,0 +1,14 @@
+package com.example.mstu1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_account);
+    }
+}
